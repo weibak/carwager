e5dc9a1e2b7b29d3dfed86bf27183fc1846aec46
Artem Sheibak <sheibakaa@gmail.com>
2022-03-29 20:04:15 +0300

    fix error

--- carwager/general/forms.py
+++ carwager/general/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=100)
     first_name = forms.CharField(max_length=200)
     last_name = forms.CharField(max_length=200)
     email = forms.EmailField()
