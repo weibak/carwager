d27232ac8327300acbbf567164abb80474f8e4fb
Artem Sheibak <sheibakaa@gmail.com>
2023-03-14 13:59:23 +0300

    minor update

--- carwager/general/settings.py
+++ carwager/general/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-import django_filters.rest_framework
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
