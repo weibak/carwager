2b0d24c099f6e91d648ad7dd15834be085f85389
Artem Sheibak <sheibakaa@gmail.com>
2023-04-28 15:38:00 +0300

    minor

--- carwager/news/views.py
+++ carwager/news/views.py
@@ -7,6 +7,7 @@ from news.models import News
 logger = logging.getLogger(__name__)
 
 
+# news page
 def news_list_all(request):
     if request.user.is_anonymous:
         return redirect("auth")
@@ -15,11 +16,13 @@ def news_list_all(request):
     return render(request, "news/news_list.html", {"news": news})
 
 
+# show current new
 def news_view(request, slug):
     news = News.objects.get(slug=slug)
     return render(request, "news/news_view.html", {"news": news})
 
 
+# create new only for admin
 def create_new(request, *args, **kwargs):
     if request.user.is_superuser:
         if request.method == "POST":
