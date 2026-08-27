dc8ad4de6f0e76b885db8809a233d98a0cf9b7b7
weibak <sheibakaa@gmail.com>
2026-08-27 16:40:29 +0300

    минор

--- carwager/auction/tasks.py
+++ carwager/auction/tasks.py
@@ -1,3 +1,4 @@
+from django.db.models import F
 from django.utils import timezone
 from django_rq import job
 import logging
@@ -13,25 +14,33 @@ def run_status_update():
     now = timezone.now()
     logger.info(f"Time: {now}. Update statuses...")
     for auction in auctions:
+        cur_state = auction.status
+        nex_state = None
         if auction.date_start <= now <= auction.date_end:
-            auction.status = "go"
-            auction.save()
+            nex_state = "go"
         if auction.date_end < now:
-            auction.status = "stop"
-            auction.save()
+            nex_state = "stop"
         if auction.date_start > now:
-            auction.status = "soon"
-            auction.save()
+            nex_state = "soon"
+        if cur_state == nex_state:
+            continue
+
+        auction.status = nex_state
+        auction.save(update_fields=['status'])
 
 
 # function to search winners in all auctions, where status is stop
 @job
 def search_winners():
-    end_auctions = Auction.objects.filter(status="stop").all()
+    end_auctions = Auction.objects.filter(status="stop").exclude(
+        bids__isnull=False)
     logger.info("Search winners...")
     for auction in end_auctions:
+        winner = Winner.objects.filter(auction=auction)
+        if winner is not None:
+            continue
         winner = auction.bids.filter(auction=auction).order_by("-created_at").first()
         if winner:
             winn = winner.user
             Winner.objects.update_or_create(auction=auction, user=winn)
-            logger.info(winn)
+            logger.info(f"For Auction id: {auction.id} - winner: user_id {winner.id}")
