44523bdf78b4eabeced8dfbf3a906b19da40fdac
weibak <sheibakaa@gmail.com>
2026-09-18 15:47:38 +0300

    fix linter

--- carwager/general/settings.py
+++ carwager/general/settings.py
@@ -413,4 +413,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
+EMAIL_PORT = os.getenv("EMAIL_PORT")
